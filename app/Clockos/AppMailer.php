8adcbffd768d63d099a25868b2b40b6556cfcd3f
Ye1LOVE <yellove1992@gmail.com>
2018-08-04 11:39:46 +0900

    delete quoto

--- app/Clockos/AppMailer.php
+++ app/Clockos/AppMailer.php
@@ -16,7 +16,7 @@ class AppMailer
 {
     protected $mailer;
 
-    protected $from = 'env('MAIL_FROM_ADDRESS', 'info@fuckb.at')';
+    protected $from = env('MAIL_FROM_ADDRESS', 'info@fuckb.at');
 
     protected $to;
 
