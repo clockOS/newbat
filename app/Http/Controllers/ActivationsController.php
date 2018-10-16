63b427794391819436f10eb10851c6591b685e08
Ye1LOVE <yellove1992@gmail.com>
2018-10-16 12:14:54 +0900

    Update ActivationsController.php

--- app/Http/Controllers/ActivationsController.php
+++ app/Http/Controllers/ActivationsController.php
@@ -39,7 +39,7 @@ class ActivationsController extends Controller
 
         Auth::user()->assignRole('rookie');
 
-        return redirect('auth/login');
+        return redirect("/newbie");
 
     }
 }
