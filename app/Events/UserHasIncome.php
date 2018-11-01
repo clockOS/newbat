996a0a7c3fce72c92585043e49f0324b0019a1ce
Ye1LOVE <yellove1992@gmail.com>
2018-11-01 14:16:47 +0900

    Update UserHasIncome.php

--- app/Events/UserHasIncome.php
+++ app/Events/UserHasIncome.php
@@ -5,19 +5,22 @@ namespace App\Events;
 use App\Events\Event;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Income;
 
 class UserHasIncome extends Event
 {
     use SerializesModels;
+    
+    public $income;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Income $income)
     {
-        //
+        $this->income = $income;
     }
 
     /**
