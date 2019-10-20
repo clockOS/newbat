bb06f4a0b11099cce0600556865c55df3d4a5231
Ye1LOVE <5682707+clockOS@users.noreply.github.com>
2019-10-20 20:22:45 +0900

    Update confirm.blade.php

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>激活FuckBAT账号</title>
+    <title>激活{{trans('app.brand')}}账号</title>
 </head>
 <body>
-    <h1>欢迎加入到FuckBAT</h1>
+    <h1>欢迎加入到{{trans('app.brand')}}</h1>
     <p><a href="{{ url("register/confirm/{$token}") }}">点击此链接激活账号</a></p>
     <br/>若无法点击请复制链接到在游览器中打开
     <br/>{{ url('register/confirm/'.$token) }}
