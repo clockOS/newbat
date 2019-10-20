b86885e3055faeca711f594ff5f2c48f78ee9f7d
Ye1LOVE <5682707+clockOS@users.noreply.github.com>
2019-10-20 20:08:36 +0900

    Update AppMailer.php

--- app/Clockos/AppMailer.php
+++ app/Clockos/AppMailer.php
@@ -16,12 +16,12 @@ class AppMailer
 {
     protected $mailer;
 
-    //protected $from = config('mail.from.address');
+    protected $from = config('mail.from.address');
 
     //protected $from ='info@fuckb.at';
 
 
-    protected $from = env('MAIL_FROM_ADDRESS','info@fuckb.at');
+
 
     protected $to;
 
