deb6a62907787fe75a37c156fa9fb1d80ba83518
Ye1LOVE <5682707+clockOS@users.noreply.github.com>
2020-06-10 09:08:37 +0900

    Update contact.blade.php

--- resources/views/about/contact.blade.php
+++ resources/views/about/contact.blade.php
@@ -3,8 +3,5 @@
     <h1>{{trans('app.contact')}}</h1><hr/>
     Email: pingdhf@gmail.com<br/>
     Slack: <a href="https://join.slack.com/t/dahaofang/shared_invite/zt-eeg756g7-hpRwgPXrv2JJgJYNTt9Lhg">Join Slack</a> <br/>
-    电报: <a href="https://t.me/joinchat/AAAAAE5dBakuhUBvFHHVlw">电报频道<br/>
-    微信: pingdhf
-    YouTube: https://youtube.com/dahaofang
-    推特：https://twitter.com/dahaofang
+    微信: pingdhf <br/>
 @stop
